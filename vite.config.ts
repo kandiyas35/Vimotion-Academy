c39ba3a346123096667ef777ed0726b2c234f44f
Kandiyas35 <ibnukandiyas35@gmail.com>
2025-09-15 11:39:57 +0700

    Update fitur baru / perbaikan2

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/Vimotion-Academy/',
+  // --- penting untuk GitHub Pages ---
+  base: "/Vimotion-Academy/",
 
   server: {
     host: "::",
     port: 8080,
   },
 
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    // aktifkan componentTagger hanya saat development
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
 
   resolve: {
     alias: {
